29a0f16f67c26e5f572491fcfa4d5bec4180fdc5
mix irving <whimful@gmail.com>
2015-07-14 01:05:46 +1200

    fix keepawake, add more facilitators, some stlying and copy

--- keepAwake.js
+++ keepAwake.js
@@ -4,7 +4,7 @@ module.exports = function keepAwake() {
   setInterval(
     function() {
       console.log('POKE')
-      http.get("https://facilawho.herokuapp.com");
+      http.get("http://facilawho.herokuapp.com");
     }, 
     300000 // every 5 minutes (300000)
   ) 
